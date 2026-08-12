e388772fdb71b1198d64b44861b92ff680817275
ahmedbnramadan <ahmedbnramadan11@gmail.com>
2026-08-12 07:25:55 +0200

    Added icons to DVLD.csproj and changed the UI to inclue the new icons

--- DVLD/frmMain.cs
+++ DVLD/frmMain.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
 using Business;
 
@@ -14,6 +15,7 @@ namespace DVLD
         private ToolStripMenuItem _menuDrivers;
         private ToolStripMenuItem _menuUsers;
         private ToolStripMenuItem _menuAccountSettings;
+
         private StatusStrip _ssFooter;
         private ToolStripStatusLabel _lblCurrentUser;
 
@@ -24,6 +26,9 @@ namespace DVLD
             IsMdiContainer = true;
             WindowState = FormWindowState.Maximized;
             StartPosition = FormStartPosition.CenterScreen;
+
+            BackColor = Color.White;
+            ForeColor = Color.Black;
 
             // Build the menu and status bar
             _InitializeMainMenu();
@@ -37,22 +42,30 @@ namespace DVLD
         {
             _msMain = new MenuStrip();
 
-            _msMain.BackColor = Color.FromArgb(45, 45, 48);
-            _msMain.ForeColor = Color.White;
+            // Light menu appearance
+            _msMain.BackColor = Color.White;
+            _msMain.ForeColor = Color.FromArgb(45, 45, 48);
+
+            _msMain.ImageScalingSize = new Size(24, 24);
 
             // Main menu items
             _menuApplications = new ToolStripMenuItem("Applications");
+            _menuApplications.Image = LoadIcon("Applications 64.png");
 
             _menuPeople = new ToolStripMenuItem("People");
+            _menuPeople.Image = LoadIcon("People 64.png");
             _menuPeople.Click += _OpenPeopleForm;
 
             _menuDrivers = new ToolStripMenuItem("Drivers");
+            _menuDrivers.Image = LoadIcon("Drivers 64.png");
             _menuDrivers.Click += _OpenDriversForm;
 
             _menuUsers = new ToolStripMenuItem("Users");
+            _menuUsers.Image = LoadIcon("Users 2 64.png");
             _menuUsers.Click += _OpenUsersForm;
 
             _menuAccountSettings = new ToolStripMenuItem("Account Setting");
+            _menuAccountSettings.Image = LoadIcon("account_settings 64.png");
 
             // Applications menu
             ToolStripMenuItem menuDrivingLicensesServices =
@@ -69,6 +82,22 @@ namespace DVLD
 
             ToolStripMenuItem menuManageTestTypes =
                 new ToolStripMenuItem("Manage Test Types");
+
+            // Applications icons
+            menuDrivingLicensesServices.Image =
+                LoadIcon("Driver License 48.png");
+
+            menuManageApplications.Image =
+                LoadIcon("Manage Applications 32.png");
+
+            menuDetainLicenses.Image =
+                LoadIcon("Detain 32.png");
+
+            menuManageApplicationTypes.Image =
+                LoadIcon("Application Types 64.png");
+
+            menuManageTestTypes.Image =
+                LoadIcon("Test Type 64.png");
 
             _menuApplications.DropDownItems.Add(menuDrivingLicensesServices);
             _menuApplications.DropDownItems.Add(menuManageApplications);
@@ -96,16 +125,36 @@ namespace DVLD
             ToolStripMenuItem menuRetakeTest =
                 new ToolStripMenuItem("Retake Test");
 
+            // Driving Licenses Services icons
+            menuNewDrivingLicense.Image =
+                LoadIcon("New Driving License 32.png");
+
+            menuRenewDrivingLicense.Image =
+                LoadIcon("Renew Driving License 32.png");
+
+            menuReplacementForLostorDamagedLicense.Image =
+                LoadIcon("Damaged Driving License 32.png");
+
+            menuReleaseDetainedDrivingLicense.Image =
+                LoadIcon("Release Detained License 32.png");
+
+            menuRetakeTest.Image =
+                LoadIcon("Retake Test 32.png");
+
             menuDrivingLicensesServices.DropDownItems.Add(menuNewDrivingLicense);
             menuDrivingLicensesServices.DropDownItems.Add(menuRenewDrivingLicense);
-            menuDrivingLicensesServices.DropDownItems.Add(menuReplacementForLostorDamagedLicense);
-            menuDrivingLicensesServices.DropDownItems.Add(menuReleaseDetainedDrivingLicense);
+            menuDrivingLicensesServices.DropDownItems.Add(
+                menuReplacementForLostorDamagedLicense);
+            menuDrivingLicensesServices.DropDownItems.Add(
+                menuReleaseDetainedDrivingLicense);
             menuDrivingLicensesServices.DropDownItems.Add(menuRetakeTest);
 
             // Driving License Services forms
             menuRenewDrivingLicense.Click += _RenewDrivingLicense;
-            menuReplacementForLostorDamagedLicense.Click += _ReplacementForLostorDamagedLicense;
-            menuReleaseDetainedDrivingLicense.Click += _ReleaseDetainedDrivingLicense;
+            menuReplacementForLostorDamagedLicense.Click +=
+                _ReplacementForLostorDamagedLicense;
+            menuReleaseDetainedDrivingLicense.Click +=
+                _ReleaseDetainedDrivingLicense;
             menuRetakeTest.Click += _RetakeTest;
 
             // Manage Applications
@@ -113,14 +162,26 @@ namespace DVLD
                 new ToolStripMenuItem("Local Driving License Applications");
 
             ToolStripMenuItem menuInternationalDrivingLicenseApplications =
-                new ToolStripMenuItem("International Driving License Applications");
+                new ToolStripMenuItem(
+                    "International Driving License Applications");
 
-            menuManageApplications.DropDownItems.Add(menuLocalDrivingLicenseApplications);
-            menuManageApplications.DropDownItems.Add(menuInternationalDrivingLicenseApplications);
+            menuLocalDrivingLicenseApplications.Image =
+                LoadIcon("Local 32.png");
+
+            menuInternationalDrivingLicenseApplications.Image =
+                LoadIcon("International 32.png");
+
+            menuManageApplications.DropDownItems.Add(
+                menuLocalDrivingLicenseApplications);
+            menuManageApplications.DropDownItems.Add(
+                menuInternationalDrivingLicenseApplications);
 
             // Manage Applications forms
-            menuLocalDrivingLicenseApplications.Click += _LocalDrivingLicenseApplications;
-            menuInternationalDrivingLicenseApplications.Click += _InternationalDrivingLicenseApplications;
+            menuLocalDrivingLicenseApplications.Click +=
+                _LocalDrivingLicenseApplications;
+
+            menuInternationalDrivingLicenseApplications.Click +=
+                _InternationalDrivingLicenseApplications;
 
             // Detained Licenses
             ToolStripMenuItem menuManageDetainedLicenses =
@@ -132,14 +193,30 @@ namespace DVLD
             ToolStripMenuItem menuReleaseDetainedLicense =
                 new ToolStripMenuItem("Release Detained License");
 
-            menuDetainLicenses.DropDownItems.Add(menuManageDetainedLicenses);
+            menuManageDetainedLicenses.Image =
+                LoadIcon("Detained Driving License 32.png");
+
+            menuDetainLicense.Image =
+                LoadIcon("Detain 32.png");
+
+            menuReleaseDetainedLicense.Image =
+                LoadIcon("Release Detained License 32.png");
+
+            menuDetainLicenses.DropDownItems.Add(
+                menuManageDetainedLicenses);
             menuDetainLicenses.DropDownItems.Add(menuDetainLicense);
-            menuDetainLicenses.DropDownItems.Add(menuReleaseDetainedLicense);
+            menuDetainLicenses.DropDownItems.Add(
+                menuReleaseDetainedLicense);
 
             // Detained Licenses forms
-            menuManageDetainedLicenses.Click += _ManageDetainedLicenses;
-            menuDetainLicense.Click += _DetainLicense;
-            menuReleaseDetainedLicense.Click += _ReleaseDetainedLicense;
+            menuManageDetainedLicenses.Click +=
+                _ManageDetainedLicenses;
+
+            menuDetainLicense.Click +=
+                _DetainLicense;
+
+            menuReleaseDetainedLicense.Click +=
+                _ReleaseDetainedLicense;
 
             // New Driving License
             ToolStripMenuItem menuLocalLicense =
@@ -148,8 +225,15 @@ namespace DVLD
             ToolStripMenuItem menuInternationalLicense =
                 new ToolStripMenuItem("International License");
 
+            menuLocalLicense.Image =
+                LoadIcon("Local 32.png");
+
+            menuInternationalLicense.Image =
+                LoadIcon("International 32.png");
+
             menuNewDrivingLicense.DropDownItems.Add(menuLocalLicense);
-            menuNewDrivingLicense.DropDownItems.Add(menuInternationalLicense);
+            menuNewDrivingLicense.DropDownItems.Add(
+                menuInternationalLicense);
 
             menuLocalLicense.Click += _LocalLicense;
             menuInternationalLicense.Click += _InternationalLicense;
@@ -164,12 +248,23 @@ namespace DVLD
             ToolStripMenuItem menuLogOut =
                 new ToolStripMenuItem("Log Out");
 
+            menuCurrentUserInfo.Image =
+                LoadIcon("User 32 -2.png");
+
+            menuChangePassword.Image =
+                LoadIcon("Password 32.png");
+
+            menuLogOut.Image =
+                LoadIcon("Sign Out 32.png");
+
             menuCurrentUserInfo.Click += _CurrentUserInfo;
             menuChangePassword.Click += _ChangePassword;
             menuLogOut.Click += _LogOut;
 
-            _menuAccountSettings.DropDownItems.Add(menuCurrentUserInfo);
-            _menuAccountSettings.DropDownItems.Add(menuChangePassword);
+            _menuAccountSettings.DropDownItems.Add(
+                menuCurrentUserInfo);
+            _menuAccountSettings.DropDownItems.Add(
+                menuChangePassword);
             _menuAccountSettings.DropDownItems.Add(menuLogOut);
 
             // Add main menu items
@@ -186,10 +281,28 @@ namespace DVLD
         private void _InitializeStatusBar()
         {
             _ssFooter = new StatusStrip();
-            _lblCurrentUser = new ToolStripStatusLabel("User: Admin");
+
+            _ssFooter.BackColor = Color.WhiteSmoke;
+            _ssFooter.ForeColor = Color.FromArgb(45, 45, 48);
+
+            _lblCurrentUser =
+                new ToolStripStatusLabel(
+                    "User: Admin");
 
             _ssFooter.Items.Add(_lblCurrentUser);
+
             Controls.Add(_ssFooter);
+        }
+
+        // Loads an icon from the application's Icons folder.
+        private Image LoadIcon(string fileName)
+        {
+            string path = Path.Combine(
+                Application.StartupPath,
+                "Icons",
+                fileName);
+
+            return Image.FromFile(path);
         }
 
         // Form navigation
@@ -217,7 +330,8 @@ namespace DVLD
         }
 
         // Opens a parameterized MDI child only once.
-        private void OpenMDIForm<T>(Func<T> formFactory) where T : Form
+        private void OpenMDIForm<T>(
+            Func<T> formFactory) where T : Form
         {
             foreach (Form frm in MdiChildren)
             {
@@ -255,105 +369,144 @@ namespace DVLD
 
         // Applications - Direct Forms
 
-        private void _ManageApplicationTypes(object sender, EventArgs e)
+        private void _ManageApplicationTypes(
+            object sender,
+            EventArgs e)
         {
             new frmListApplicationTypes().ShowDialog();
         }
 
-        private void _ManageTestTypes(object sender, EventArgs e)
+        private void _ManageTestTypes(
+            object sender,
+            EventArgs e)
         {
             MessageBox.Show("Coming Soon");
         }
 
         // Driving Licenses Services
 
-        private void _RenewDrivingLicense(object sender, EventArgs e)
+        private void _RenewDrivingLicense(
+            object sender,
+            EventArgs e)
         {
             new frmRenewDrivingLicense().ShowDialog();
         }
 
-        private void _ReplacementForLostorDamagedLicense(object sender, EventArgs e)
+        private void _ReplacementForLostorDamagedLicense(
+            object sender,
+            EventArgs e)
         {
             new frmReplaceLicense().ShowDialog();
         }
 
-        private void _ReleaseDetainedDrivingLicense(object sender, EventArgs e)
+        private void _ReleaseDetainedDrivingLicense(
+            object sender,
+            EventArgs e)
         {
             new frmReleaseDetainedLicense().ShowDialog();
         }
 
-        private void _RetakeTest(object sender, EventArgs e)
+        private void _RetakeTest(
+            object sender,
+            EventArgs e)
         {
             MessageBox.Show("Coming Soon");
         }
 
         // New Driving License
 
-        private void _LocalLicense(object sender, EventArgs e)
+        private void _LocalLicense(
+            object sender,
+            EventArgs e)
         {
             MessageBox.Show("Coming Soon");
         }
 
-        private void _InternationalLicense(object sender, EventArgs e)
+        private void _InternationalLicense(
+            object sender,
+            EventArgs e)
         {
             new frmAddNewInternationalLicense().ShowDialog();
         }
 
         // Manage Applications
 
-        private void _LocalDrivingLicenseApplications(object sender, EventArgs e)
+        private void _LocalDrivingLicenseApplications(
+            object sender,
+            EventArgs e)
         {
             OpenMDIForm<frmListLocalDrivingLicenseApplications>();
         }
 
-        private void _InternationalDrivingLicenseApplications(object sender, EventArgs e)
+        private void _InternationalDrivingLicenseApplications(
+            object sender,
+            EventArgs e)
         {
             MessageBox.Show("Coming Soon");
         }
 
         // Detained Licenses
 
-        private void _ManageDetainedLicenses(object sender, EventArgs e)
+        private void _ManageDetainedLicenses(
+            object sender,
+            EventArgs e)
         {
             new frmListDetainedLicenses().ShowDialog();
         }
 
-        private void _DetainLicense(object sender, EventArgs e)
+        private void _DetainLicense(
+            object sender,
+            EventArgs e)
         {
             MessageBox.Show("Coming Soon");
         }
 
-        private void _ReleaseDetainedLicense(object sender, EventArgs e)
+        private void _ReleaseDetainedLicense(
+            object sender,
+            EventArgs e)
         {
             new frmReleaseDetainedLicense().ShowDialog();
         }
 
         // Account Settings
 
-        private void _CurrentUserInfo(object sender, EventArgs e)
+        private void _CurrentUserInfo(
+            object sender,
+            EventArgs e)
         {
-            new frmShowUserInfo(clsGlobal.CurrentUserID).ShowDialog();
+            new frmShowUserInfo(
+                clsGlobal.CurrentUserID).ShowDialog();
         }
 
-        private void _ChangePassword(object sender, EventArgs e)
+        private void _ChangePassword(
+            object sender,
+            EventArgs e)
         {
             new frmChangePassword().ShowDialog();
         }
 
-        private void _LogOut(object sender, EventArgs e)
+        private void _LogOut(
+            object sender,
+            EventArgs e)
         {
             if (MessageBox.Show(
                     "Are you sure you want to log out?",
                     "Log Out",
                     MessageBoxButtons.YesNo,
-                    MessageBoxIcon.Question) == DialogResult.Yes)
+                    MessageBoxIcon.Question)
+                == DialogResult.Yes)
             {
                 Hide();
 
-                if (new frmLogin().ShowDialog() == DialogResult.OK)
+                if (new frmLogin().ShowDialog()
+                    == DialogResult.OK)
+                {
                     Show();
+                }
                 else
+                {
                     Application.Exit();
+                }
             }
         }
     }
